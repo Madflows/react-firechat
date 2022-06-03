4813e2e79bff1b82d2e287b5289053341c3df0ce
Folarin Lawal <lawalfolarin6@gmail.com>
2022-06-03 10:31:10 +0000

    🎉 Ready to Ship!

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const Button = ({ onClick = null, children = null, className = null }) => (
-  <button onClick={onClick} 
-  className={className + "rounded shadow-md pl-6 pr-8 py-3 bg-gray-100 hover:bg-slate-200 text-gray-600 font-medium flex items-center justify-center overflow-y-hidden focus:outline-none focus:ring focus:ring-primary-500 focus:ring-opacity-75"}
-  >
+  <button onClick={onClick} className={className}>
     {children}
   </button>
 );
--- src/components/Channel.jsx
+++ src/components/Channel.jsx
@@ -91,19 +91,19 @@ const Channel = ({ user = null, db = null, msgRef = null }) => {
       <div className="mb-6 mx-4">
         <form
           onSubmit={handleOnSubmit}
-          className="flex flex-row bg-gray-200 dark:bg-coolDark-400 rounded-md px-4 py-3 z-10 max-w-screen-lg mx-auto dark:text-white shadow-md"
+          className="flex flex-row bg-gray-200 dark:bg-coolDark-400 rounded-md z-10 max-w-screen-lg mx-auto dark:text-slate-800 shadow-md"
         >
           <input
             type="text"
             value={newMessage}
             onChange={handleOnChange}
             placeholder="Type your message here..."
-            className="flex-1 bg-transparent outline-none"
+            className="flex-1 bg-transparent dark:text-slate-800 outline-none px-4 py-3"
           />
           <button
             type="submit"
             disabled={!newMessage}
-            className="uppercase font-semibold text-sm tracking-wider text-gray-500 hover:text-gray-900 dark:hover:text-white transition-colors"
+            className="uppercase font-semibold text-sm tracking-wider text-gray-500 px-4 py-3 dark:hover:bg-slate-800 hover:text-gray-900 dark:hover:text-white transition-colors"
           >
             Send
           </button>
